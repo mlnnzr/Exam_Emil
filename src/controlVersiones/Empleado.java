9b1d9a9af61fd91b21c50203f1d0770bcc063d6c
1dami <1dami@HZ301502.TartangaLH.eus>
2021-05-24 09:39:14 +0200

    JavaDoc

--- src/controlVersiones/Empleado.java
+++ src/controlVersiones/Empleado.java
@@ -1,6 +1,20 @@
 package controlVersiones;
 
+/*
+ *autor Emil
+ *version Version 1, 2021
+ *since 24/05/2021
+ *
+ */
+
 public class Empleado {
+	
+	/*
+	 * Nombre: Nombre del empleado
+	 * Apellidos: apellidos del empleado
+	 * salario: salario del empleado
+	 */
+	
 	private String nombre;
 	private String apellidos;
 	private double salario; 
@@ -13,27 +27,61 @@ public Empleado(String nombre, String apellido, double salario) {
 }
 
 
-
+/*
+ * recoge el nombre del empleado
+ */
 public String getNombre() {
 	return nombre;
 }
+
+
+/*
+ * escribe el nombre del empleado
+ */
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
+
+
 public String getApellidos() {
+	/*
+	 * recoge los apellidos del empleado
+	 */
+	
 	return apellidos;
 }
+
 public void setApellidos(String apellidos) {
+/*
+ * escribe los apellidos del empleado
+ */
+	
 	this.apellidos = apellidos;
 }
+
+
 public double getSalario() {
+	/*
+	 * recoge el salario del empleado
+	 */
 	return salario;
 }
+
+
 public void setSalario(double salario) {
+	/*
+	 * recoge el salario del empleado
+	 */
 	this.salario = salario;
 }
 
+
+
 public void subirsalario (double subida) {
+	/*
+	 * este metodo añade la subida al salario de un trabajador,
+	 * y lo vuelve el nuevo salario del mismo
+	 */
 	salario=salario+subida;
 }
 
